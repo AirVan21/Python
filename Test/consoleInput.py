0ea5d9148c90c17ed15afeb8ee46d249acae0d11
AirVan21 <airvan21@gmail.com>
2014-09-21 20:26:09 +0400

    Input + string

---
+++ Test/consoleInput.py
@@ -0,0 +1,8 @@
+# Please, write you name!
+
+name = raw_input("- Hi, what's you name? \n- ")
+print "- Nice to meet you, " + name + "!"
+
+# Using string slice, all letters except first
+ame = name[1:len(name)]
+print "- " + ame 
