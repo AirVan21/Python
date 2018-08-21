178be0c19618bcd213528806d133e379506516af
Ivan Abramov <ivan.Abramov@itiviti.com>
2018-08-21 10:30:00 +0300

    Deleted old py scripts

--- Test/consoleInput.py
+++
@@ -1,8 +0,0 @@
-# Please, write you name!
-
-name = raw_input("- Hi, what's you name? \n- ")
-print "- Nice to meet you, " + name + "!"
-
-# Using string slice, all letters except first
-ame = name[1:len(name)]
-print "- " + ame 
--- Test/ifClause.py
+++
@@ -1,8 +0,0 @@
-print "Testing if clause :"
-
-statement = (1 == 0)
-
-if statement :
-	print "Statement is true!"
-else:
-	print "Statemetn is false!"
